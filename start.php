40c5baae14a839bea048fb193afcb7d27a071e90
Arthur Souza Armond <90006665+ZzSouzazZ@users.noreply.github.com>
2022-11-02 13:41:29 -0300

    version 0.0.3

--- start.php
+++ start.php
@@ -39,18 +39,22 @@
                                     display:flex;
                                     align-items:center;
                                     border-radius:12px;
+                                    margin:0px;
                                 }
                                 #img-product-not{
                                     width:100%;
                                 }
                                 #title-product-not{
-                                    font-weight:800;
+                                    font-weight:bold;
                                     font-family:'Montserrat';
                                     font-size:18px;
                                 }
                                 #text-sell-not{
                                     font-family:'Poppins';
                                     font-size:15px;
+                                    color:grey;
+                                    line-height:1.2em;
+                                    margin-top:-2px;
                                 }
                                 @keyframes fade {
                                     0%{
@@ -66,6 +70,14 @@
                                 .teste{
                                     opacity: 0;
                                     animation: fade 3s linear;
+                                }
+                                @media only screen and (max-width: 768px) {
+                                    .container-notificacao{
+                                        width:100%;
+                                    }
+                                    #text-sell-not{
+                                        font-size:13px;
+                                    }
                                 }
                             </style>
                         </head>
@@ -126,4 +138,179 @@
                     HTML;
         echo $html;
 
+    }
+
+    add_shortcode('notification_venda_carrinho','notificacao_cart');
+
+    function notificacao_cart(){
+
+        $html = <<< HTML
+                        <head>
+                            <style>
+                                @keyframes fade-in {
+                                    0%{
+                                        opacity: 0;
+                                    }
+                                    70%{
+                                        opacity:0;
+                                    }
+                                    100%{
+                                        opacity:1;
+                                    }
+                                }
+                                .container-cart-notification{
+                                    width:40%;
+                                    padding:10px;
+                                    background-color:#fff;
+                                    border-radius:10px;
+                                    position:fixed;
+                                    bottom:0;
+                                    left:15px;
+                                    animation: fade-in 4s linear;
+                                }
+                                .text-title{
+                                    font-weight:700;
+                                    font-size:20px;
+                                    color:red;
+                                }
+                                .text-paragraph{
+                                    margin-top:8px;
+                                    color:#252525;
+                                    line-height:1.2em;
+                                    font-size:18px;
+                                    font-weight:500;
+                                }
+                                #timer{
+                                    color:red;
+                                    font-weight:600;
+                                }
+                                .divider{
+                                    height:2px;
+                                    background-color:#d7d7d7;
+                                    width:20%;
+                                }
+                                @media only screen and (max-width: 768px){
+                                    .container-cart-notification{
+                                        width:100vw;
+                                    }
+                                    .text-paragraph{
+                                        font-size:15px;
+                                    }
+                                }
+
+                            </style>
+                        </head>
+                        <body>
+                            <div class="container-cart-notification">
+                                <h3 class="text-title">
+                                    Atenção!
+                                </h3>
+                                <div class='divider'></div>
+                                <p class="text-paragraph">
+                                    Você possui apenas <a id="timer"></a> minutos para finalizar sua compra. <br><a style='color:grey;font-size:13px;'>Após isso, seu carrinho será resetado.</a>
+                                </p>
+                            </div>
+                            <script>
+                                function startTimer(duration, display) {
+                                    var timer = duration, minutes, seconds;
+                                    setInterval(function () {
+                                        minutes = parseInt(timer / 60, 10);
+                                        seconds = parseInt(timer % 60, 10);
+                                        minutes = minutes < 10 ? "0" + minutes : minutes;
+                                        seconds = seconds < 10 ? "0" + seconds : seconds;
+                                        display.textContent = minutes + ":" + seconds;
+                                        if (--timer < 0) {
+                                            timer = duration;
+                                        }
+                                    }, 1000);
+                                }
+                                window.onload = function () {
+                                    var duration = 60 * 10; // Converter para segundos
+                                        display = document.querySelector('#timer'); // selecionando o timer
+                                    startTimer(duration, display); // iniciando o timer
+                                };
+                            </script>
+                        </body>
+                    HTML;
+        
+        echo $html;
+    }
+
+    add_shortcode('notification-venda-produto','notificacao_product');
+
+    function notificacao_product(){
+
+        $html = <<< HTML
+                        <head>
+                            <style>
+                                @keyframes fade-in {
+                                    0%{
+                                        opacity: 0;
+                                    }
+                                    70%{
+                                        opacity:0;
+                                    }
+                                    100%{
+                                        opacity:1;
+                                    }
+                                }
+                                .container-product-notification{
+                                    padding:10px;
+                                    bottom:0;
+                                    left:15px;
+                                    background-color:#fff;
+                                    position:fixed;
+                                    width:40%;
+                                    border-radius:10px;
+                                    animation: fade-in 4s linear
+                                }
+                                .title-notification-product{
+                                    color:orange;
+                                    font-size:20px;
+                                    font-weight:600;
+                                }
+                                #people-count{
+                                    color:orange;
+                                }
+                                .text-notification-product{
+                                    font-size:18px;
+                                    color:#252525;
+                                    font-weight:500;
+                                    margin-top:5px;
+                                }
+                                .divider{
+                                    height:1.5px;
+                                    background-color:grey;
+                                    width:25%;
+                                }
+                                @media only screen and (max-width: 768px){
+                                    .container-product-notification{
+                                        width:100%;
+                                    }
+                                    .divider{
+                                        width:40%
+                                    }
+                                    .text-notification-product{
+                                        font-size:15px;
+                                    }   
+                                }
+                            </style>
+                        </head>
+                        <body>
+                            <div class="container-product-notification">
+                                <h3 class="title-notification-product">
+                                    Produto quente!
+                                </h3>
+                                <div class="divider"></div>
+                                <p class="text-notification-product">
+                                    Outras <span id="people-count"></span> estão vendo este produto agora. <span style='color:orange;font-size:15px;'>Reserve já o seu!.</span>
+                                </p>
+                            </div>  
+                            <script>
+                                var peopleCount = Math.floor(Math.random() * 31);
+                                document.getElementById("people-count").innerHTML = peopleCount+" pessoas";
+                            </script>
+                        </body>
+                    HTML;
+        echo $html;
     }
